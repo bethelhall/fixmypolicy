3613c270e6b402c493e232faa1d3ce012cea92ef
bethelhall <bethelroger33@gmail.com>
2025-06-23 22:01:51 -0400

    request generator added

--- FL/Experiment-1/bash.sh
+++ FL/Experiment-1/bash.sh
@@ -1,15 +1,14 @@
 POLICY_NUM=$1
-#GET THE SECOND ARGUMENT FROM THE COMMAND LINE
 POLICY_NUM_SECOND=$2
 QUACKY_SRC_DIR="/home/bhall2/Documents/fixmypolicy/quacky/src"
 
-POLICY_FILE="/home/bhall2/Documents/fixmypolicy/FL/Experiment-1/Results/repaired_${POLICY_NUM}.json"
+POLICY_FILE="/home/bhall2/Documents/fixmypolicy/FL/Experiment-1/original_policy/${POLICY_NUM}.json"
 REQUEST_FILE="/home/bhall2/Documents/fixmypolicy/FL/Experiment-1/requests/${POLICY_NUM_SECOND}.json"
 QUACKY_OUTPUT_DIR="/home/bhall2/Documents/fixmypolicy/FL/Experiment-1/Results/Quacky_output"
 
 OUTPUT_FILE_PATH="${QUACKY_OUTPUT_DIR}/${POLICY_NUM_SECOND}.txt"
 
-cd "$QUACKY_SRC_DIR" || { echo "Error: Could not change to $QUACKY_SRC_DIR"; exit 1; }
+cd "$QUACKY_SRC_DIR" || { echo "Error: Could not c  hange to $QUACKY_SRC_DIR"; exit 1; }
 
 python3 validate_requests.py -p1 "$POLICY_FILE" --requests "$REQUEST_FILE" -s > "$OUTPUT_FILE_PATH"
 echo "Output saved to $OUTPUT_FILE_PATH".
